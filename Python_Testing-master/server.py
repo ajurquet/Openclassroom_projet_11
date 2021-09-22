5abb73421c13908c7c4094ac8948af84e3b3734b
Alexandre JURQUET <a.jurquet@gmail.com>
2021-09-22 11:39:03 +0200

    bug solved

--- Python_Testing-master/server.py
+++ Python_Testing-master/server.py
@@ -5,12 +5,12 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 def loadClubs():
     with open('clubs.json') as c:
         listOfClubs = json.load(c)['clubs']
-        return listOfClubs
+    return listOfClubs
 
 def loadCompetitions():
     with open('competitions.json') as comps:
         listOfCompetitions = json.load(comps)['competitions']
-        return listOfCompetitions
+    return listOfCompetitions
 
 
 app = Flask(__name__)
@@ -32,11 +32,20 @@ def showSummary():
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
+def book(competition, club):
 
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+    clubs_list = []
+    for clb in clubs:
+        if clb['name'] == club:
+            clubs_list.append(clb)
+    foundClub = clubs_list[0]
 
+    competitions_list = []
+    for cmp in competitions:
+        if cmp['name'] == competition:
+            competitions_list.append(cmp)
+    foundCompetition = competitions_list[0]
+    
     if foundClub and foundCompetition:
         return render_template('booking.html', club=foundClub, competition=foundCompetition)
     else:
@@ -46,11 +55,25 @@ def book(competition,club):
 
 @app.route('/purchasePlaces',methods=['POST'])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
+   
+    competitions_list = []
+    for comp in competitions:
+        if comp['name'] == request.form['competition']:
+            competitions_list.append(comp)
+    competition = competitions_list[0]
+   
+    clubs_list = []
+    for clb in clubs:
+        if clb['name'] == request.form['club']:
+            clubs_list.append(clb)
+    club = clubs_list[0]
+    
+    placesRequired = int(request.form['places'])  # correspond au nombres de places voulues entrées dans le formulaire
 
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+    club["points"] = int(club["points"]) - placesRequired
+    # c'est ici qu'il faut enlever dans places dans le json
+
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
