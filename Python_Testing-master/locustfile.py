1795b7560ad1c0fdc6c03bf6999dd1c430d4c47d
Alexandre JURQUET <a.jurquet@gmail.com>
2021-10-06 15:55:13 +0200

    test locust fonctionnent

--- Python_Testing-master/locustfile.py
+++ Python_Testing-master/locustfile.py
@@ -1,6 +1,5 @@
-import time
+import json
 from locust import HttpUser, task, between
-
 
 from tests.test_server import competitions, clubs
 
@@ -10,119 +9,24 @@ class LocustTest(HttpUser):
 
     @task
     def index(self):
-        self.client.get("http://127.0.0.1:5000")
+        self.client.get("/")
 
     @task
     def competition_booking_url_is_online(self):
-        self.client.get("http://127.0.0.1:5000/book/Spring%20Festival/Simply%20Lift")
+        self.client.get("/book/Spring%20Festival/Simply%20Lift")
        
     @task
-    def test_booking_a_competition(self):
-        response = self.client.get("http://127.0.0.1:5000/purchasePlaces", data={"places": "4", "club": "Test", "competition": "Test Festival 2018"})
-        
-    # @task
-    # def test_booking_a_competition(self):
-    #     self.client.post("http://127.0.0.1:5000/purchasePlaces", data={"places": "4"},
-    #                                                                  clubs={"name":"Test",
-    #                                                                     "email":"test@test.com",
-    #                                                                     "points":"20"
-    #                                                                 },
-    #                                                                 competitions = {"name": "Test Festival_2025",
-    #                                                                 "date": "2025-03-27 10:00:00",
-    #                                                                 "numberOfPlaces": "25"
-    #                                                             })
+    def booking_a_competition(self):
+        response = self.client.post("/purchasePlaces", data={"places": "1", "club": "Simply Lift", "competition": "Spring Festival"})
+        print(response)
+
+    @task
+    def go_to_board(self):
+        self.client.get("/board")
            
-         
+
     def on_start(self):
-        self.client.post("http://127.0.0.1:5000/showSummary", data={'email': 'test@test.com'})
+        self.client.post("/showSummary", data={'email': 'john@simplylift.co'})
 
     def on_stop(self):
-        self.client.get("http://127.0.0.1:5000/logout")
-    
-        
-
-
-
-
-
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-
-# @app.route('/showSummary',methods=['POST'])
-# def showSummary():
-#     try:
-#         club = [club for club in clubs if club['email'] == request.form['email']][0]
-#     except IndexError:
-#         return redirect(url_for('index'))
-#     else:
-#         return render_template('welcome.html',club=club, competitions=competitions)
-          
-
-# @app.route('/book/<competition>/<club>')
-# def book(competition, club):
-
-#     clubs_list = []
-#     for clb in clubs:
-#         if clb['name'] == club:
-#             clubs_list.append(clb)
-#     foundClub = clubs_list[0]
-
-#     competitions_list = []
-#     for cmp in competitions:
-#         if cmp['name'] == competition:
-#             competitions_list.append(cmp)
-#     foundCompetition = competitions_list[0]
-    
-#     if foundClub and foundCompetition:
-#         return render_template('booking.html', club=foundClub, competition=foundCompetition)
-#     else:
-#         flash("Something went wrong-please try again")
-#         return render_template('welcome.html', club=club, competitions=competitions)
-
-
-# @app.route('/purchasePlaces',methods=['POST'])
-# def purchasePlaces():
-   
-#     competitions_list = []
-#     for comp in competitions:
-#         if comp['name'] == request.form['competition']:
-#             competitions_list.append(comp)
-#     competition = competitions_list[0]
-   
-#     clubs_list = []
-#     for clb in clubs:
-#         if clb['name'] == request.form['club']:
-#             clubs_list.append(clb)
-#     club = clubs_list[0]
-    
-#     placesRequired = int(request.form['places'])  # correspond au nombres de places voulues entrées dans le formulaire
-    
-#     global places_booked_counter
-#     places_booked_counter += int(request.form['places'])
-
-#     if places_booked_counter > 12 :
-#         flash("You can't book more than 12 places in a competition")
-#     elif int(request.form['places']) > int(club["points"]):
-#         flash("You don't have enough points")
-#     else:
-#         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
-#         club["points"] = int(club["points"]) - placesRequired     
-#         flash('Great-booking complete!')
-#     return render_template('welcome.html', club=club, competitions=competitions)
-
-# # Ils devraient voir un message confirmant le nombre de places achetées, ou un
-# # message indiquant que le concours est complet. Les points utilisés doivent être
-# # déduits du total précédent.
-
-
-# @app.route('/board')
-# def board():
-#     return render_template('board.html',clubs=clubs)
-
-
-# @app.route('/logout')
-# def logout():
-#     return redirect(url_for('index'))
+        self.client.get("/logout")
